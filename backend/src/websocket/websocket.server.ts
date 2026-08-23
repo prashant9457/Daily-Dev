f68b5db24fd2005abd2069c678755f29c33b275c
Prashant <prasantsingh945@gmail.com>
2026-08-23 19:12:42 +0530

    authenticate webSocket Request for user

--- backend/src/websocket/websocket.server.ts
+++ backend/src/websocket/websocket.server.ts
@@ -1,5 +1,8 @@
+import { parseCookie } from "cookie";
+import { verify } from "crypto";
 import http from "http";
 import { WebSocketServer } from "ws";
+import { verifyToken } from "../utils/verifyToken.js";
 
 export function setupWebSocket(server: http.Server) {
     const wss = new WebSocketServer({
@@ -7,7 +10,26 @@ export function setupWebSocket(server: http.Server) {
     });
 
     server.on('upgrade', (request, socket, head) => {
-        console.log('WebSocket upgrade requested');
+        const cookies = parseCookie(request.headers.cookie ?? "");
+
+        const token = cookies.token;
+
+        if(!token) {
+            socket.write("HTTP/1.1 401 Unauthorized");
+            socket.destroy();
+            return;
+        }
+
+        try {
+
+            const decoded = verifyToken(token);
+            console.log("WebSocket authenticated: ", decoded._id);
+
+        } catch {
+            socket.write("HTTP/1.1 401 Unauthorized");
+            socket.destroy();
+            return;
+        }
     });
 
     wss.on('connection', (socket, request) => {
